209737da82d4cdf3e897c01bc36660f37a93764c
nabilmohamed99 <mr.nabilmohamed@gmail.com>
2024-03-24 12:13:34 +0000

    correction de dernier Lint

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -31,5 +31,3 @@ class ModelTests(TestCase):
         for email, expected in sample_emails:
             user = get_user_model().objects.create_user(email, 'sample123')
             self.assertEqual(user.email, expected)
-
-
