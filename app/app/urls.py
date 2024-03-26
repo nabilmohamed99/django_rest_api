2f200286fefa238a396ae33e8f413b678b7ca027
nabilmohamed99 <mr.nabilmohamed@gmail.com>
2024-03-26 01:33:17 +0000

    add the drf_spectacular for openapi documenation and swagger

--- app/app/urls.py
+++ app/app/urls.py
@@ -15,7 +15,20 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # generer le fichier yaml de shema
+    path('/api/schema', SpectacularAPIView.as_view(), name='api-schema'),
+    # swagger documentation
+    path(
+        'api/docs',
+        SpectacularSwaggerView.as_view(url_name="api-schema"),
+        name='api-docs',
+        )
+
 ]
